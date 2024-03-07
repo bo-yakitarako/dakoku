b19f415bc15ca137777ab3cdfc91b7bad77d2e03
bo-yakitarako <55777096+bo-yakitarako@users.noreply.github.com>
2024-03-07 18:07:19 +0900

    muiいれた

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,17 @@
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 export const App: React.FC = () => {
-  return <div>ｵﾊﾖｳ、ｵﾆｲﾁｬﾝ</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      ｵﾊﾖｳ、ｵﾆｲﾁｬﾝ
+    </ThemeProvider>
+  );
 };
