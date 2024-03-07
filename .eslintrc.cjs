d094e9304d3bf9ed743c324c4584394f074d7fb0
bo-yakitarako <55777096+bo-yakitarako@users.noreply.github.com>
2024-03-07 17:19:11 +0900

    いにしゃらいずんだもん

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    '@electron-toolkit/eslint-config-ts/recommended',
+    '@electron-toolkit/eslint-config-prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        tabWidth: 2,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        singleQuote: true,
+        semi: true,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
