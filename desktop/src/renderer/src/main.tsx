38137fc77c9f081f0dd5d9e2aabe4a5806930737
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2024-12-09 01:44:41 +0900

    さよならアンドロメダ

--- desktop/src/renderer/src/main.tsx
+++ desktop/src/renderer/src/main.tsx
@@ -4,18 +4,5 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { WorkStatus } from '../../preload/dataType';
-
-type StorageKey = 'count' | 'times';
-
-declare global {
-  interface Storage {
-    workStatus?: WorkStatus;
-    works?: string;
-    getItem(key: StorageKey): string | null;
-    setItem(key: StorageKey, value: string): void;
-    removeItem(key: StorageKey): void;
-  }
-}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
