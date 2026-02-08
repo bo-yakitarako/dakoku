7db378a78084c3ef6290f429404a2237c00b6d2c
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2026-02-09 04:50:07 +0900

    組閣に成功した

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,5 @@
 import { config } from 'dotenv';
-import { User } from './db/User';
 
 config();
 
-(async () => {
-  const user = await User.create({ name: 'ぼかすか', email: 'komanechi@baka.com' });
-  console.log(user.createdAt.format('YYYY-MM-DD HH:mm:ss'));
-})();
+console.log(process.env.MONGO_URI);
