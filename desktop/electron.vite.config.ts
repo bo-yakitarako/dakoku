d62c8388e82ee35a14752232929d9948e96618a9
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2026-03-25 16:01:54 +0900

    やったぜ。

--- desktop/electron.vite.config.ts
+++ desktop/electron.vite.config.ts
@@ -1,47 +1,59 @@
 import { resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-  main: {
-    resolve: {
-      alias: {
-        '@': resolve('src'),
-        '@resources': resolve('resources'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const viteApiOrigin = env.VITE_API_ORIGIN ?? 'http://localhost:8080';
+
+  return {
+    main: {
+      resolve: {
+        alias: {
+          '@': resolve('src'),
+          '@resources': resolve('resources'),
+        },
       },
-    },
-    plugins: [
-      externalizeDepsPlugin({
-        exclude: ['electron-store'],
-      }),
-    ],
-  },
-  preload: {
-    resolve: {
-      alias: {
-        '@': resolve('src'),
-        '@resources': resolve('resources'),
+      define: {
+        'process.env.VITE_API_ORIGIN': JSON.stringify(viteApiOrigin),
       },
+      plugins: [
+        externalizeDepsPlugin({
+          exclude: ['electron-store'],
+        }),
+      ],
     },
-    plugins: [externalizeDepsPlugin()],
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@': resolve('src'),
-        '@resources': resolve('resources'),
+    preload: {
+      resolve: {
+        alias: {
+          '@': resolve('src'),
+          '@resources': resolve('resources'),
+        },
       },
+      define: {
+        'process.env.VITE_API_ORIGIN': JSON.stringify(viteApiOrigin),
+      },
+      plugins: [externalizeDepsPlugin()],
     },
-    plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'src/renderer/index.html'),
-          calendar: resolve(__dirname, 'src/renderer/calendar.html'),
-          dayDetail: resolve(__dirname, 'src/renderer/dayDetail.html'),
-          auth: resolve(__dirname, 'src/renderer/auth.html'),
+    renderer: {
+      resolve: {
+        alias: {
+          '@': resolve('src'),
+          '@resources': resolve('resources'),
+        },
+      },
+      plugins: [react()],
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'src/renderer/index.html'),
+            calendar: resolve(__dirname, 'src/renderer/calendar.html'),
+            dayDetail: resolve(__dirname, 'src/renderer/dayDetail.html'),
+            auth: resolve(__dirname, 'src/renderer/auth.html'),
+          },
         },
       },
     },
-  },
+  };
 });
